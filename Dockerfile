6eb2dd7f520d555b9fbbedce27c67dc1aaf5ccb8
Takehiko Esaka <take.pbfzbf302@gmail.com>
2021-10-23 11:38:53 +0900

    line-botおうむ返し

--- Dockerfile
+++ Dockerfile
@@ -1,30 +1,9 @@
-# Define function directory
-ARG FUNCTION_DIR="/function"
-
-#FROM python:3.8-slim
 FROM public.ecr.aws/lambda/python:3.8
 
-# Install aws-lambda-cpp build dependencies
-# RUN apt-get update && \
-#   apt-get install -y \
-#   g++ \
-#   make \
-#   cmake \
-#   unzip \
-#   libcurl4-openssl-dev \
-#   libsm6 \
-#   libxrender1 \
-#   libxtst6 \
-#   python3-pip
-
-#COPY ./app.py ${FUNCTION_DIR}/app.py
 COPY ./app.py ./
 
 #ADD ./webapp/requirements.txt /tmp/requirements.txt
 #RUN pip install --no-cache-dir -q -r /tmp/requirements.txt
 
-# Set working directory to function root directory
-#WORKDIR /function
-
-#ENTRYPOINT [ "/usr/local/bin/python", "-m", "awslambdaric" ]
-CMD [ "app.handler" ]
+# ここで実行するlambdaの関数名を指定 
+CMD [ "app.handler" ] 
--- app.py
+++ app.py
@@ -1,12 +1,35 @@
 import json
+import urllib.request
 
 def handler(event, context):
-    return {
-        "statusCode": 200,
-        "body": json.dumps(
-            {
-                "message": "hello world",
-            }
-        ),
-    }
+    for message_event in json.loads(event['body'])['events']:
+        url = 'https://api.line.me/v2/bot/message/reply'
+        headers = {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + "NhMQQyqeSZsjUWLEaXgMa0TX9+L7siWTQWbrAPR++Q4uLnkOrMQuLtjDzl7epKePB6s21qBgiB9qvthwqoeyn/qI7iTg+0pa9ZTncfbo/UrXM4D264PTEkwq8mG/l74EeZjAbrU947+R/sVYnX+AjgdB04t89/1O/w1cDnyilFU="
+        }
+        
+        message = get_message()
+        
+        body = {
+            'replyToken': message_event['replyToken'],
+            'messages': [
+                {
+                    "type": "text",
+                    "text": message,
+                }
+            ]
+        }
 
+        req = urllib.request.Request(url, data=json.dumps(body).encode('utf-8'), method='POST', headers=headers)
+        
+        with urllib.request.urlopen(req) as res:
+            logger.info(res.read().decode("utf-8"))
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
+    
+def get_message():
+    return "hello world"
