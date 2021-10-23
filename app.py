519ffbae7c4e2e71e784b26639457b7f1df29906
OsugiTakuya <t.4.103.tkynt@gmail.com>
2021-10-23 11:56:05 +0900

    app.py変更（bot未マージ）

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
 import json
 import urllib.request
+from logging import getLogger
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), 'webapp'))
+from dialogue_system.bot import Bot
+
+bot = Bot()
+logger = getLogger(__name__)
+
 
 def handler(event, context):
     for message_event in json.loads(event['body'])['events']:
@@ -9,7 +18,7 @@ def handler(event, context):
             'Authorization': 'Bearer ' + "NhMQQyqeSZsjUWLEaXgMa0TX9+L7siWTQWbrAPR++Q4uLnkOrMQuLtjDzl7epKePB6s21qBgiB9qvthwqoeyn/qI7iTg+0pa9ZTncfbo/UrXM4D264PTEkwq8mG/l74EeZjAbrU947+R/sVYnX+AjgdB04t89/1O/w1cDnyilFU="
         }
         
-        message = get_message()
+        message = get_message(message_event['message']['text'])
         
         body = {
             'replyToken': message_event['replyToken'],
@@ -31,5 +40,7 @@ def handler(event, context):
         'body': json.dumps('Hello from Lambda!')
     }
     
-def get_message():
-    return "hello world"
+def get_message(text):
+    '''ユーザの入力をbotに渡し、返事を取得する'''
+    reply = bot.reply(text)
+    return reply
