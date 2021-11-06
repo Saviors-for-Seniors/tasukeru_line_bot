7454679d90d345c95a3477c981a0f8314394a232
OsugiTakuya <t.4.103.tkynt@gmail.com>
2021-11-06 15:39:27 +0900

    言語生成部をルールベース用に修正

--- webapp/dialogue_system/language_generation/generator.py
+++ webapp/dialogue_system/language_generation/generator.py
@@ -4,7 +4,6 @@ import sys
 
 class LanguageGenerator(object):
 
-    """
     # 3クラスお試し用URL
     URL_DICT = {'足が悪い': ['https://healthrent.duskin.jp/products/hokouhojyo/B-4-24/index.html?link=se_colum_pro_in',
                              'https://www.cainz.com/shop/g/g4936695745505/?utm_content=85&utm_term=&utm_medium=cpc&utm_source=google&utm_campaign=hl_mi_di_al_00_ssc&gclid=Cj0KCQjw_dWGBhDAARIsAMcYuJypt8ejxUrpWKGwR_EBf-6N57I7wlgv8uiVk60nVCDc7UWQ6Y7v51MaAlFZEALw_wcB'],
@@ -12,7 +11,6 @@ class LanguageGenerator(object):
                              'https://www.yodobashi.com/product/000000224701012473/?gad1=&gad2=g&gad3=&gad4=452596090545&gad5=15771747885033500943&gad6=&gclid=Cj0KCQjw_dWGBhDAARIsAMcYuJxF4hc65OEa-q1RRTmdr0xQvLwp_bUkSu40q1GPVfT104uWjniSd84aAmBvEALw_wcB&xfr=pla'],
                 '認知症': ['https://www.amazon.co.jp/Romi-%E3%82%B3%E3%83%9F%E3%83%A5%E3%83%8B%E3%82%B1%E3%83%BC%E3%82%B7%E3%83%A7%E3%83%B3%E3%83%AD%E3%83%9C%E3%83%83%E3%83%88-ROMI-P02-%E9%80%9A%E5%B8%B8%E8%B2%A9%E5%A3%B2-%E3%83%9E%E3%83%83%E3%83%88%E3%83%9B%E3%83%AF%E3%82%A4%E3%83%88/dp/B08H2HL2J7/ref=asc_df_B08H2HL2J7/?tag=jpgo-22&linkCode=df0&hvadid=342578596485&hvpos=&hvnetw=g&hvrand=8439001825582924048&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=1009337&hvtargid=pla-1146895915392&psc=1&tag=&ref=&adgrpid=71561406449&hvpone=&hvptwo=&hvadid=342578596485&hvpos=&hvnetw=g&hvrand=8439001825582924048&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=1009337&hvtargid=pla-1146895915392',
                            'https://item.rakuten.co.jp/monorogu/php82190/?gclid=Cj0KCQjw_dWGBhDAARIsAMcYuJzP7DBR33yT7dSmBux-8DGF9J91TAWPwhaZPdyx31UrGIO3bZSlEjEaAleSEALw_wcB&scid=af_pc_etc&sc2id=af_113_0_10001868&icm_agid=54982841894&icm_cid=1425341487&icm_acid=834-739-7270&gclid=Cj0KCQjw_dWGBhDAARIsAMcYuJzP7DBR33yT7dSmBux-8DGF9J91TAWPwhaZPdyx31UrGIO3bZSlEjEaAleSEALw_wcB']}
-    """
     """
     # 40クラスお試し用URL
     import os
@@ -40,7 +38,6 @@ class LanguageGenerator(object):
         
         # URLを提示する場合
         if sys_act_type == 'SUGGEST_GOODS':
-            """
             # PROBLEMにクラスラベルのリストが入っている場合
             sent = ''
             for problem in dialogue_act['PROBLEM']:
@@ -50,11 +47,10 @@ class LanguageGenerator(object):
                 info = {'商品{}'.format(i+1): url for i, url in enumerate(url_list)}
                 for name, url in info.items():
                     sent += '\n{}\n\t{}'.format(name, url)
-            """
             # PROBLEMにクラスラベルと確率の辞書が入っている場合
-            sent = ''
-            for problem, probable in dialogue_act['PROBLEM'].items():
-                sent += '\n{} :  {:.5f}'.format(problem, probable)
+            # sent = ''
+            # for problem, probable in dialogue_act['PROBLEM'].items():
+            #     sent += '\n{} :  {:.5f}'.format(problem, probable)
 
         # デフォルトメッセージ
         elif sys_act_type == 'DEFAULT_MESSAGE':
